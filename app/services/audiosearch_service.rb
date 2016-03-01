e00e8f11999868a37f32711c5b224b90234f71f1
Nicole Hall <nicolealexandrahall@gmail.com>
2016-03-01 16:04:38 -0700

    created audiosearch service

---
+++ app/services/audiosearch_service.rb
@@ -0,0 +1,7 @@
+class Audiosearch_Service
+
+  conn = Faraday.new(:url => 'http://sushi.com') do |faraday|
+    faraday.request  :url_encoded             # form-encode POST params
+    faraday.response :logger                  # log requests to STDOUT
+    faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+  end
