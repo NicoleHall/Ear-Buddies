bfe78da8ac9cc41c447ba4b482976616746f1a07
Nicole Hall <nicolealexandrahall@gmail.com>
2016-03-01 19:11:20 -0700

    deleted podcast model

--- test/integration/user_logs_in_with_twitter_test.rb
+++ test/integration/user_logs_in_with_twitter_test.rb
@@ -5,10 +5,10 @@ class UserLogsInWithTwitterTest < ActionDispatch::IntegrationTest
   test "logging in" do
     visit "/"
     assert_equal 200, page.status_code
-    click_link "login"
+    click_link "Login"
     assert_equal "/", current_path
     assert page.has_content?("Cole")
-    assert page.has_link?("logout")
+    assert page.has_link?("Logout")
   end
 
 
