836226d830827d97b5089d0bb6e692067b85192c
Nicole Hall <nicolealexandrahall@gmail.com>
2016-03-05 17:43:45 -0700

    added Search Again button to playlist show page

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -2,11 +2,13 @@ class PodcastsController < ApplicationController
   before_action :set_playlist, only:[:search_result, :new, :destroy]
   before_action :store_query, only:[:search_result]
 
+
   def search
     #@podcast_subject = params["podcast"]["subject"]
   end
 
   def search_result
+
     # session[:subject] = params["podcast_subject"]["subject"]
     @results = AudiosearchService.new.search_by_query(params["podcast_subject"]["subject"])
   end
@@ -17,7 +19,7 @@ class PodcastsController < ApplicationController
     @podcast = Podcast.create_podcast_on_a_playlist(params.merge({
       playlist_id: @playlist.id}))
     if @podcast.save
-    
+
       redirect_to playlist_path(@podcast.playlist)
     else
       flash[:notice] = "Fail Whale"
