04e1114aae7b3b132078d2e0c384ee4d543e136a
Nicole Hall <nicolealexandrahall@gmail.com>
2016-03-07 19:34:45 -0700

    recongigured test helper so simplecov works properly

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
+ENV['RAILS_ENV'] ||= 'test'
 require 'simplecov'
 SimpleCov.start 'rails'
-
-ENV['RAILS_ENV'] ||= 'test'
+puts "required simplecov"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/pride'
