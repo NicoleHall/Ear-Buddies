904aa1433783c6de48481f9447d8f40e96b04ea8
Nicole Hall <nicolealexandrahall@gmail.com>
2016-03-08 16:53:14 -0700

    added happy path and sad path for user controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "#show displays user info" do
+    user = users(:one)
+    session[:user_id] = user.id
+    get :show
+
+    assert_response :success
+  end
+
+  test "#redirect if no current user" do
+    user = users(:one)
+    get :show
+
+    assert_response :found
+  end
 end
