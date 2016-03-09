70c3700ec25556fe5ce2aa218207ed45a4de7cd7
Nicole Hall <nicolealexandrahall@gmail.com>
2016-03-09 12:24:23 -0700

    removed comments from playlist controller

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,6 +1,4 @@
 class PlaylistsController < ApplicationController
-  #before_action :selector, except: :show, except: :new, except: :create
-  #before_action :selector, only: [:edit]
 
   def index
     @playlists = current_user.playlists
@@ -28,16 +26,4 @@ class PlaylistsController < ApplicationController
     Playlist.delete(params[:id])
     redirect_to playlists_path
   end
-
-#private
-  # def selector
-  #   playlist = Playlist.find(params[:id])
-  #   session[:playlist] = playlist
-  #   if playlist.podcasts == []
-  #     redirect_to podcasts_search_path
-  #   else
-  #   redirect_to :controller=>'podcasts',:action=>'search_result'
-    # redirect_to podcasts_search_result_path, action: 'POST'
-  #   end
-  # end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,13 +8,8 @@ require 'minitest/pride'
 require 'capybara/rails'
 require 'webmock/minitest'
 
-
 class ActiveSupport::TestCase
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
 
   VCR.configure do |config|
     config.ignore_localhost = true
@@ -50,6 +45,24 @@ class ActiveSupport::TestCase
       playlist_id: 45,
       audiosearch_id: 8001
     )
+  end
+
+  def create_cole
+    User.create(
+     id: 3,
+     name: "Cole Hall",
+     screen_name: "ColeMerscih",
+     uid: "12345",
+     image: "http://pbs.twimg.com/profile_images/610989157108707328/eiwDUICE_normal.jpg",
+     oauth_token: 12345,
+     oauth_token_secret: 12345)
+  end
+
+  def create_coles_playlist
+    Playlist.create(
+     id: 49,
+     subject: "HotDogs#3",
+     user_id: 3)
   end
 end
 
