06f1dfba43960d9b95c82f8c69c2db71cbddc890
Nicole Hall <nicolealexandrahall@gmail.com>
2016-03-09 11:48:50 -0700

    moved create_podcast into a method in the test helper

--- test/integration/user_can_add_podcast_to_playlist_test.rb
+++ test/integration/user_can_add_podcast_to_playlist_test.rb
@@ -6,16 +6,7 @@ class UserAddAPodcastToAPlaylistTest < ActionDispatch::IntegrationTest
     VCR.use_cassette('localhost') do
       create_user_and_playlist
       assert_equal 2, User.find(1).podcasts.count
-      result = Podcast.new(
-      ep_title: "Are Hot Dogs Sandwiches?",
-      description:  "When it comes to the correct definition of a sandwich, I’m a strict constructionist.",
-      date_created_at: "2014-06-26",
-      show_title: "The Sporkful",
-      url: "https://www.audiosear.ch/media/64eeb39222985854216d4418983b2a4c/0/public/audio_file/50866/.mp3",
-      image: "https://s3.amazonaws.com/www-prod-popuparchive/the-sporkful.BACjFQ.popuparchive.org/thumb_mza_6297250918464517177.600x600-75.jpg?X-Amz-Expires=604800&X-Amz-Date=20160305T140233Z&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJ6B3Q7DFQGUS2TXQ/20160305/us-east-1/s3/aws4_request&X-Amz-SignedHeaders=host&X-Amz-Signature=651562e8e5f3954d4cab10cf094a9b26ba290039b3f00a3f5748ab5ad88fa23b",
-      playlist_id: 45,
-      audiosearch_id: 8001
-      )
+      result = create_podcast
       assert_equal 0, Playlist.find(49).podcasts.count
       visit podcasts_search_path
       fill_in("podcast_subject[subject]", :with => 'dogs')
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,19 +23,33 @@ class ActiveSupport::TestCase
   end
 
   def create_user_and_playlist
-    cole = User.create(
-      id: 3,
-      name: "Cole Hall",
-      screen_name: "ColeMerscih",
-      uid: "12345",
-      image: "http://pbs.twimg.com/profile_images/610989157108707328/eiwDUICE_normal.jpg",
-      oauth_token: 12345,
-      oauth_token_secret: 12345)
 
-      hotdogs_3 = Playlist.create(
-        id: 49,
-        subject: "HotDogs#3",
-        user_id: 3)
+    User.create(
+     id: 3,
+     name: "Cole Hall",
+     screen_name: "ColeMerscih",
+     uid: "12345",
+     image: "http://pbs.twimg.com/profile_images/610989157108707328/eiwDUICE_normal.jpg",
+     oauth_token: 12345,
+     oauth_token_secret: 12345)
+
+    Playlist.create(
+     id: 49,
+     subject: "HotDogs#3",
+     user_id: 3)
+  end
+
+  def create_podcast
+     Podcast.new(
+      ep_title: "Are Hot Dogs Sandwiches?",
+      description:  "When it comes to the correct definition of a sandwich, I’m a strict constructionist.",
+      date_created_at: "2014-06-26",
+      show_title: "The Sporkful",
+      url: "https://www.audiosear.ch/media/64eeb39222985854216d4418983b2a4c/0/public/audio_file/50866/.mp3",
+      image: "https://s3.amazonaws.com/www-prod-popuparchive/the-sporkful.BACjFQ.popuparchive.org/thumb_mza_6297250918464517177.600x600-75.jpg?X-Amz-Expires=604800&X-Amz-Date=20160305T140233Z&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJ6B3Q7DFQGUS2TXQ/20160305/us-east-1/s3/aws4_request&X-Amz-SignedHeaders=host&X-Amz-Signature=651562e8e5f3954d4cab10cf094a9b26ba290039b3f00a3f5748ab5ad88fa23b",
+      playlist_id: 45,
+      audiosearch_id: 8001
+    )
   end
 end
 
